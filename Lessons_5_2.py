087b87dd9ff201820e87430ae7135c0ce784d90a
Flagman2015 <87610375+Flagman2015@users.noreply.github.com>
2021-08-02 23:12:51 +1000

    Домашняя работа_5

---
+++ Lessons_5_2.py
@@ -0,0 +1,7 @@
+with open("text.txt") as file:
+    file_lines = file.readlines()
+    for num, line in enumerate(file_lines):
+        words_count = len(line.split())
+        print(f"#{num + 1} - {words_count} words")
+
+
---
+++ lessons_5_3.py
@@ -0,0 +1,13 @@
+with open ('text2.txt') as file:
+    file_lines = file.readlines()
+    employees = {}
+    sum_salary = 0
+    for line in file_lines:
+        emp_info = line.split()
+        employees.update({emp_info[0]: float(emp_info[1])})
+        sum_salary += float(emp_info[1])
+average_sal = sum_salary / len(employees)
+print(f"average = {average_sal}")
+for k, v in employees.items():
+    if v < average_sal:
+        print(f"{k}: {v}")
